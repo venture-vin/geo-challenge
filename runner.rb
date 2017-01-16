15dde598812ece14097a9033f6a32375525797ba
Olya Royall <olya.vgrig+git@gmail.com>
2017-01-15 22:22:43 -0800

    update store filter logic to return proper address

--- runner.rb
+++ runner.rb
@@ -1,22 +1,25 @@
 require_relative 'store_parser'
-require_relative 'address_converter'
 require 'geocoder'
 
 
 def store_filter(address)
 all = parse_from_store_file('store-locations.csv')
 home = Geocoder.coordinates(address)
-min_distance = 0
+distances = []
 
-  all.each do |coord|
+  all.each_with_index do |coord, index|
+    address = coord[:address]
+    state = coord[:state]
     destination = [coord[:latitude], coord[:longitude]]
-    distance = Geocoder::Claculations.distance_between(destination, home)
-    min_distance = distance
-    opt_distance = [min_distance, distance].min
+    distance = Geocoder::Calculations.distance_between(destination, home)
+    distances << distance
   end
+
+  index = distances.rindex(distances.min)
+  return "#{all[index][:address]} #{all[index][:state]} #{all[index][:zip_code]}"
+
 # compare current location with other lat/longs
 # if distance less than 1 mile return results
-return opt_distance
 end
 
-# puts store_filter("1770 Union St, San Francisco, CA 94115")
+puts store_filter("1770 Union St, San Francisco, CA 94115")
