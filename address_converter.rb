22c51beb87290843cfe5017300bfe7815bd55416
Olya Royall <olya.vgrig+git@gmail.com>
2017-01-15 21:23:29 -0800

    update runner code with Geocoder

--- address_converter.rb
+++
@@ -1,7 +0,0 @@
-require 'rubygems'
-require 'geokit'
-
-def convert_address_to_coordinates(location)
-  coords = Geokit::Geocoders::MultiGeocoder.geocode(location)
-  return coords.lat, coords.lng
-end
--- runner.rb
+++ runner.rb
@@ -1,16 +1,16 @@
 require_relative 'store_parser'
 require_relative 'address_converter'
+require 'geocoder'
 
 
 def store_filter(address)
 all = parse_from_store_file('store-locations.csv')
-home = convert_address_to_coordinates(address).join(',')
-current_address = Geokit::LatLng.new(home)
+home = Geocoder.coordinates(address)
 min_distance = 0
 
-  all.each do |x, y|
-    destination = '#{x[:latitude]}, #{y[:longitude]}'
-    distance = current_address.distance_to(destination)
+  all.each do |coord|
+    destination = [coord[:latitude], coord[:longitude]]
+    distance = Geocoder::Claculations.distance_between(destination, home)
     min_distance = distance
     opt_distance = [min_distance, distance].min
   end
